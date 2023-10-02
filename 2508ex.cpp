bf9f83b04bab46ace0f2769123bc416ce9da54fe
loramazer <lauratopoma@gmail.com>
2023-10-02 17:54:41 -0300

    Funções em C
    Funções de fatorial, fibonacci, maior valor de um vetor e soma de todos os números de um vetor.

---
+++ 2508ex.cpp
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+int fatorial (int);
+int fibonacci (int);
+float maiorvalor(float [], int);
+float somavet(float [], int);
+
+int main (){
+	int n, i, N=5;
+	float V;
+	printf ("Insira um numero inteiro: \n");
+	scanf("%d", &n);
+	printf ("o fatorial deste numero: %d\n", fatorial(n));
+	printf ("o enesimo numero da sequencia fibonacci: %d\n", fibonacci(n));
+	printf ("o maior valor deste vetor: %f\n", maiorvalor(V, 5));
+	printf ("a soma dos numeros deste vetor: %f\n", somavet(V, 5));
+	return 0;
+}
+
+int fatorial (int n) {
+	if (n==1)
+		return 1;
+	else 
+		return (n*fatorial(n-1));
+}
+
+int fibonacci (int n) {
+	if (n==1 || n==2)
+		return 1;
+	else return (fibonacci(n-1)+fibonacci(n-2));
+}
+float maiorvalor (float V[], int N) {
+	float aux;
+	if(N==1)
+		return V[0];
+	aux=maiorvalor(V, N-1);
+	if (aux>V[N-1])
+		return aux;
+	else return (V[N-1]);
+}
+float somavet (float V[], int tamanho) {
+	//criterio de parada 
+	if (tamanho==0) return 0;
+	//caso recursivo soma o elemento atual com a soma dos restantes
+	return V[tamanho-1]+somavet(V, tamanho-1);
+}
+
