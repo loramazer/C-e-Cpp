3f91d3ce77840c0b3ec90b49c3b1cca7a968ff14
loramazer <lauratopoma@gmail.com>
2023-10-13 13:34:26 -0300

    Update estoque.cpp

--- estoque.cpp
+++ estoque.cpp
@@ -1,2 +1,19 @@
 // Registro para armazenar dados de produtos de um estoque (codigo, descriçaõ, quantidade,valor,data 
 // de aquisição e consumo ultimos 6 meses
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+struct data {
+	int dia, mes, ano;
+};
+struct prod {
+	int codigo;
+	char desc;
+	float qtd, valor;
+	struct data dtaq;
+	float consumo[6];
+};
+
+
