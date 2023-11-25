084983e8e3cf1d4114f44a785971727f40c56268
loramazer <lauratopoma@gmail.com>
2023-11-25 12:17:34 -0300

    Update jogoadvinhacao.cpp
    Adicionei niveis para jogar

--- jogoadvinhacao.cpp
+++ jogoadvinhacao.cpp
@@ -1,17 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#define MAX_TENTATIVAS 10
+
 
 int main () {
-    int tentativas, numero_secreto, numero_grande;
-    /* Utilizando a função time para sempre obter uma seed diferente para a função rand*/
+    int tentativas, numero_secreto, numero_grande, nivel, max_tentativas, ganhou=0;    /* Utilizando a função time para sempre obter uma seed diferente para a função rand*/
 
     srand(time(NULL));
     numero_grande = rand() ;
     numero_secreto = numero_grande % 100; /*resultado desta conta vai sempre dar um numero entre 0 e 99*/
 
-    for (tentativas=0; tentativas<=MAX_TENTATIVAS; tentativas ++) {
+printf("Escolha o nivel que quer jogar: \n");
+printf("(1) Facil    (2)Medio    (3)Dificil\n");
+scanf("%d", &nivel);
+    switch (nivel){
+        case 1: max_tentativas = 20; break;
+        case 2: max_tentativas = 10; break;
+        case 3: max_tentativas = 6; break;
+        default: printf("Escolha incorreta!");
+    }
+
+    for (tentativas=0; tentativas<max_tentativas; tentativas ++) {
         int chute;
         printf ("------------------------------\n");
         printf("Tentativa %d\n", tentativas+1);
@@ -20,6 +29,7 @@ int main () {
             if (chute>0){
                 if (chute == numero_secreto){
                     printf("Voce acertou em %d tentativas!\n", tentativas+1);
+                    ganhou = 1;
                     break;
                 }
                 else if (chute < numero_secreto){
@@ -34,7 +44,7 @@ int main () {
                 tentativas--;   
             }
     }
-    if (tentativas > MAX_TENTATIVAS){
+    if (!ganhou){
         printf("Você perdeu! O número secreto era %d\n", numero_secreto);
     }
     return 0;
