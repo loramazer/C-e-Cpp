846c1dbbaa1f880b0d32a345cb294ca7812cbfcb
loramazer <lauratopoma@gmail.com>
2023-11-29 13:45:56 -0300

    Update forcasolo.cpp
    base for functions

--- Alura/forcasolo.cpp
+++ Alura/forcasolo.cpp
@@ -6,23 +6,31 @@
 int main() {
 		char Palavra[Tam], Chute[Tam], Letra;
 		int aux, acertou, tentativas=0, acertos=0, tamanho;
+		
+		//jogador 1 escolhe palavra
 		printf ("Jogo da Forca!!\n");
 		printf("Jogador 1, insira a palara:");
 		gets(Palavra);
 		tamanho=strlen(Palavra);
+		
+		
 		for(aux=0; aux<tamanho; aux++){
 			Chute[aux]='_';
 		}		
 		Chute[aux]='\0';
+		
+		
 		while (tentativas<6 && acertos<tamanho){
+			//letra
 			printf("\n JOGO DA FORCA\n");
 			printf("%s", Chute);
 			printf("  Tentativas restantes: %d\n", 6-tentativas);
 			printf("Digite uma letra: ");
 			scanf("%c", &Letra);
+			
 			acertou=0;
 			for (aux=0; aux<tamanho; aux++) {
-			
+				//verifica se acertou
 				if (Palavra[aux]==Letra && Chute[aux]!=Letra){
 					Chute[aux]=Letra;
 					acertos++;
