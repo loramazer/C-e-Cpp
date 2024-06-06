728c2a980b4e59004b0881678516a0427112c8af
loramazer <lauratopoma@gmail.com>
2024-06-06 13:32:25 -0300

    aula arvore binaria e heaps

--- Marcio/arvoreBuscaString.cpp
+++ Marcio/arvoreBuscaString.cpp
@@ -2,15 +2,43 @@
 #include <string.h>
 using namespace std;
 
-struct no (
+struct no {
     char dado [50];
     int contador;
     struct no* esq;
     struct no* dir;
-    );
+};
     struct no* raiz = NULL;
+    char palavra_maior[50];
+    int maior;
 
-    void insere(char valor){}
+    void insere(char valor[]){
+        struct no * atual, *novo, *anterior;
+
+        novo = new(struct no);
+        strcpy(novo -> dado, valor);
+        novo -> esq = NULL;
+        novo -> dir = NULL;
+        novo -> contador = 1;
+
+        atual = raiz;
+
+        if(atual == NULL){
+            raiz = novo;
+            return;
+        }
+        while(atual != NULL){
+            anterior = atual;
+            if(strcmp(valor, atual -> dado) == 0){
+                delete(novo);
+                atual -> contador++;
+                return;
+            }
+            if(strcmp(valor, anterior -> dado) <0){
+                anterior -> esq = novo;
+            } else anterior -> dir = novo;
+        }
+    }
 
 //calcula palavra q aparece mais vezes
     void emOrdem (struct no *atual){
